d49a9f511b1453a936488b054db5c7424c3b199f
Sahil Chandra <sahil@mirpuri.in>
2023-04-08 21:48:27 +0530

    Move to airtable

--- api/doha_api.py
+++ api/doha_api.py
@@ -1,14 +1,24 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from db.kabir_dohas import dohas
+# from db.kabir_dohas import dohas
+from airtable import Airtable
+import os
 import random
 
 app = Flask(__name__)
 CORS(app, origins=["https://bnjr.github.io"])
 
+AIRTABLE_API_KEY = os.environ.get("AIRTABLE_API_KEY")
+AIRTABLE_BASE_ID = os.environ.get("AIRTABLE_BASE_ID")
+AIRTABLE_TABLE_NAME = "Dohas"
+
+airtable = Airtable(AIRTABLE_BASE_ID, AIRTABLE_TABLE_NAME, api_key=AIRTABLE_API_KEY)
+
 @app.route("/doha", methods=["GET"])
 def get_doha():
-    selected_doha = random.choice(dohas)
+    records = airtable.get_all()
+    selected_record = random.choice(records)
+    selected_doha = selected_record["fields"]
     return jsonify(selected_doha)
 
 if __name__ == "__main__":
