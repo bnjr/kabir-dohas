153e462b97a9d07b2f905b5275c4040edfbbd6a6
bnjr <sahil@mirpuri.in>
2023-04-09 02:08:16 +0530

    made it look nicer (#2)
    Added random fetch
    Added button
    Added spinner

--- src/pages/api/doha.ts
+++ src/pages/api/doha.ts
@@ -1,37 +1,53 @@
-// pages/api/doha.ts
+import {NextApiRequest, NextApiResponse} from 'next'
+import Airtable from 'airtable'
 
-import type { NextApiRequest, NextApiResponse } from 'next';
-import Airtable from 'airtable';
+const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY
+const AIRTABLE_BASE_ID = process.env.AIRTABLE_BASE_ID ?? ''
+const AIRTABLE_TABLE_NAME = 'Dohas'
 
-// Get your Airtable API key and base ID from environment variables
-const apiKey = process.env.AIRTABLE_API_KEY;
-const baseId = process.env.AIRTABLE_BASE_ID;
+const base = new Airtable({apiKey: AIRTABLE_API_KEY}).base(AIRTABLE_BASE_ID)
 
-// Connect to your Airtable
-const base = new Airtable({ apiKey }).base(baseId as string);
+let lastRandomOffset: number | null = null
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  try {
-    const records = await base('Dohas')
-      .select({
-        maxRecords: 1,
-        view: 'Grid view',
-      })
-      .firstPage();
+const getRandomDoha = async () => {
+  const totalRecords = await base(AIRTABLE_TABLE_NAME)
+    .select({
+      maxRecords: 1,
+      view: 'Grid view',
+      fields: ['Count'],
+      sort: [{field: 'Count', direction: 'desc'}],
+    })
+    .all()
 
-    const record = records[0];
-    const dohaData = {
-      Doha: record.get('Doha'),
-      EN: record.get('EN'),
-      Meaning: record.get('Meaning'),
-    };
+  const total = (totalRecords[0].fields['Count'] as number) ?? 0
 
-    res.status(200).json(dohaData);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Error fetching doha data' });
+  let randomOffset
+  do {
+    randomOffset = Math.floor(Math.random() * (total - 1)) + 1
+  } while (randomOffset === lastRandomOffset)
+  lastRandomOffset = randomOffset
+  
+  const records = await base(AIRTABLE_TABLE_NAME)
+    .select({
+      maxRecords: 1,
+      view: 'Grid view',
+      fields: ['Doha', 'EN', 'Meaning'],
+      filterByFormula: `{Count} = ${randomOffset}`,
+    })
+    .all()
+
+  return records[0].fields
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'GET') {
+    try {
+      const doha = await getRandomDoha()
+      res.status(200).json(doha)
+    } catch (error) {
+      res.status(500).json({error: 'Failed to fetch doha'})
+    }
+  } else {
+    res.status(405).json({error: 'Method not allowed'})
   }
 }
---
+++ src/pages/components/spinner.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Spinner = () => {
+  return (
+    <div className="w-8 h-8 border-t-2 border-blue-500 border-solid rounded-full animate-spin"></div>
+  );
+};
+
+export default Spinner;
