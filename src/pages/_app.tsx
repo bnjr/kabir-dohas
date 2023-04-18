e809afb024c370b10eb13324dda82b25eb448133
Sahil Chandra <sahil@mirpuri.in>
2023-04-18 19:39:00 +0530

    add component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,6 +15,7 @@ export default function App({Component, pageProps}: AppProps) {
       <FavoriteProvider>
         <MainLayout>
           <Component {...pageProps} />
+          <Analytics />
         </MainLayout>
       </FavoriteProvider>
     </AuthProvider>
