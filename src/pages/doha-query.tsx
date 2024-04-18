b004a5f7570dae03ba7f2a823c49a93c5937aa94
bnjr <sahil@mirpuri.in>
2024-04-18 13:39:19 +0530

    new page for search (#27)

--- src/pages/doha-query.tsx
+++ src/pages/doha-query.tsx
@@ -1,34 +1,30 @@
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import SEOHead from '@/components/SEO/SEOHead'
-import HomePageButtons from '@/components/Page/HomePageButtons'
-import Doha from '@/components/Doha/Doha'
 import Spinner from '@/components/Utils/Spinner'
-import SearchBar from '@/components/Find/SearchBar'
+import SynopsisCard from '@/components/Find/SynopsisCard'
 import useFetchDohas from '@/hooks/useFetchDohas'
-import { DohaData } from '@/types/types'
+import SearchBar from '@/components/Find/SearchBar'
 
-const Home = () => {
-  const [dohaData, setDohaData] = useState<DohaData | null>(null)
+const DohaQueryPage = () => {
   const [searchInput, setSearchInput] = useState<string>('')
-  const [foundDohas, setFoundDohas] = useState<DohaData[]>([])
-  const { loading, error, fetchRandomDoha } = useFetchDohas()
+  const { loading, error, synopsis, fetchDohasFromFinder } = useFetchDohas()
   const router = useRouter()
 
   useEffect(() => {
-    const randomDoha = async () => {
-      const doha = await fetchRandomDoha()
-      if (doha) setDohaData(doha)
+    const fetchData = async () => {
+      const queryParam = router.query.searchQuery
+      if (queryParam) {
+        // Ensure that queryParam is a string before passing it to fetchDohasFromFinder
+        const searchQuery = Array.isArray(queryParam)
+          ? queryParam[0]
+          : queryParam
+        setSearchInput(searchQuery)
+        await fetchDohasFromFinder(searchQuery)
+      }
     }
-    if (router.query.randomDoha === 'true') randomDoha()
-  }, [router.query.randomDoha])
-
-  const handleGetRandomDoha = async () => {
-    setFoundDohas(() => [])
-    setDohaData(null)
-    const doha = await fetchRandomDoha()
-    if (doha) setDohaData(doha)
-  }
+    fetchData()
+  }, [router.query.searchQuery])
 
   const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchInput(e.target.value)
@@ -36,12 +32,13 @@ const Home = () => {
 
   const handleAskKabir = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+    // Update the URL with the new search query
     router.push(`/doha-query?searchQuery=${encodeURIComponent(searchInput)}`)
   }
 
   return (
     <>
-      <SEOHead />
+      <SEOHead title="Search Results" />
       {error ? (
         <div className="text-red-500 text-center my-4">
           An error occurred: {error}
@@ -63,29 +60,15 @@ const Home = () => {
             <div className="flex justify-center items-center mt-4">
               <Spinner />
               <p className="text-xl font-semibold text-indigo-200 ml-4">
-                Thinking...
+                Searching...
               </p>
             </div>
           )}
-          {foundDohas.length > 0 && (
-            <h2 className="text-xl font-bold mb-4 text-indigo-200">
-              Reference Dohas...
-            </h2>
-          )}
-          {foundDohas.map((doha) => (
-            <Doha
-              key={dohaData?.id}
-              dohaData={doha}
-              loading={loading}
-              details
-            />
-          ))}
-          {dohaData && <Doha dohaData={dohaData} loading={loading} details />}
-          <HomePageButtons fetchRandomDoha={handleGetRandomDoha} />
+          {synopsis && <SynopsisCard synopsis={synopsis} />}
         </>
       )}
     </>
   )
 }
 
-export default Home
+export default DohaQueryPage
